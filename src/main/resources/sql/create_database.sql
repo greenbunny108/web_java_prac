51d82b2f63fa4c66a2ad9e16713b6fb26f8b3afa
greenbunny108 <mpopov@ispras.ru>
2023-09-28 17:48:00 +0300

    database changes order

--- src/main/resources/sql/create_database.sql
+++ src/main/resources/sql/create_database.sql
@@ -1,29 +1,4 @@
-CREATE TYPE gender AS ENUM ('male', 'female');
-CREATE TYPE relationship AS ENUM ('spouse', 'child');
-
-CREATE TABLE person (
-id            bigserial                     PRIMARY KEY,
-first_name    text                          NOT NULL,
-last_name     text                          NOT NULL,
-gender_type        gender                        NOT NULL,
-birth_date    timestamp without time zone   NOT NULL,
-death_date    timestamp without time zone,
-description   text,
-address_id    bigint REFERENCES address (id),
-CHECK (death_date is NULL OR birth_date < death_date)
-);
-
-CREATE TABLE relation (
-id                  bigserial PRIMARY KEY,
-person1_id          bigint REFERENCES person (id),
-person2_id          bigint REFERENCES person (id),
-relationship_type   relationship NOT NULL,
-start_date          timestamp without time zone   NOT NULL,
-end_date            timestamp without time zone,
-CHECK (end_date is NULL OR start_date < end_date),
-CHECK (person1_id != person2_id)
-);
-
+DROP TABLE IF EXISTS address CASCADE;
 CREATE TABLE address (
 id                  bigserial   PRIMARY KEY,
 street_address      text,
@@ -31,4 +6,31 @@ city                text,
 state_province      text,
 country             text
 );
+
+DROP TABLE IF EXISTS person CASCADE;
+CREATE TABLE person (
+id            bigserial                     PRIMARY KEY,
+first_name    text                          NOT NULL,
+last_name     text                          NOT NULL,
+gender_type   text                        NOT NULL,
+birth_date    timestamp without time zone   NOT NULL,
+death_date    timestamp without time zone,
+description   text,
+address_id    bigint REFERENCES address (id) ON DELETE CASCADE,
+CHECK (death_date is NULL OR birth_date < death_date)
+);
+
+DROP TABLE IF EXISTS relation CASCADE;
+CREATE TABLE relation (
+id                  bigserial PRIMARY KEY,
+person1_id          bigint REFERENCES person (id) ON DELETE CASCADE,
+person2_id          bigint REFERENCES person (id) ON DELETE CASCADE,
+relationship_type   integer NOT NULL,
+start_date          timestamp without time zone   NOT NULL,
+end_date            timestamp without time zone,
+CHECK (end_date is NULL OR start_date < end_date),
+CHECK (person1_id != person2_id)
+);
+
+
 
